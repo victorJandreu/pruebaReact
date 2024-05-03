bd804a2635d06062a1c9c2b8ee0d4e81ed1480ff
fuego44 <victorandreum@gmail.com>
2024-05-03 20:48:09 +0200

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from './Button'
 
 export default function App(){
     return (
         <>
             <h1>hola mundo</h1>
+            <Button>hola juapo</Button>
         </>
     )
 }
